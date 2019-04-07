463c3685941de70e988146d4f096dd14b3ce4f89
Gál Péter <pepyta118@gmail.com>
2019-04-07 23:29:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -351,7 +351,7 @@ setInterval(() => {
 		for(var i = 0; i < result["reminders"].length; i++){
 			if(!result["reminders"][i]["reminded"]){
 				console.log("Reminded!")
-				if(new Date(result["reminders"][i]["date"]) < new Date() && new Date(result["reminders"][i]["date"]) > new Date()-(5*60*1000)){
+				if(new Date(result["reminders"][i]["date"]) < new Date() && new Date(result["reminders"][i]["date"]) > (new Date()-(5*60*1000))){
 					result["reminders"][i]["reminded"] = true
 					updateData("reminders.json", "reminders", result["reminders"]).then(function(){
 						console.log(`Reminded: ${result["reminders"][i]["name"]}`)
