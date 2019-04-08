c8f9db234b00cd4fb8cbcb02c508834f352c619c
Gál Péter <pepyta118@gmail.com>
2019-04-08 18:09:01 +0200

    Revert "Updated some outdated javascript find method parameters"
    This reverts commit 3e0c1fc7ecdc6bb01f31f4eb6b682e933366074a.

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ bot.on('message', message => {
 
 					// Binds admins to the channel too
 					channel.overwritePermissions(
-						channel.guild.roles.find(user => user.name = "Adminisztrátor"),
+						channel.guild.roles.find("name", "Adminisztrátor"),
 						{
 							"VIEW_CHANNEL": true,
 							"SEND_MESSAGES": true,
@@ -156,7 +156,7 @@ bot.on('message', message => {
 
 					// Binds admins to the channel too
 					channel.overwritePermissions(
-						channel.guild.roles.find(user => user.name = "HelpBot"),
+						channel.guild.roles.find("name", "HelpBot"),
 						{
 							"VIEW_CHANNEL": true,
 							"SEND_MESSAGES": true,
@@ -326,7 +326,6 @@ bot.on('message', message => {
 					message.guild.createChannel(`társalgó`, "text")
 					.then(channel => {
 						channel.setParent(category.id)
-						channel.send(`Üdvözöllek a **saját szobádban**!\n\nAmennyiben meg szeretnéd hívni a barátaidat, azt a \`!meghív\` paranccsal tudod tenni.\nHasználat: \`!meghív JátékosNév\``)
 						.then(function(){
 							message.guild.createChannel(`beszélgető`, "voice")
 							.then(voice => {
@@ -354,7 +353,7 @@ bot.on('message', message => {
 
 									// Binds admins to the channel too
 									category.overwritePermissions(
-										channel.guild.roles.find(user => user.name = "HelpBot"),
+										channel.guild.roles.find("name", "HelpBot"),
 										{
 											"VIEW_CHANNEL": true,
 											"SEND_MESSAGES": true,
@@ -372,7 +371,7 @@ bot.on('message', message => {
 									if(result["rooms"] == undefined){
 										result["rooms"] = []
 									}
-
+									
 									result["rooms"].push(elem)
 
 									updateData("rooms.json", "rooms", result["rooms"]).then(function(){
